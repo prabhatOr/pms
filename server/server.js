510738077d105f1101088db54bbd14334f077e46
Prabhat Singh <prabhat6914@gmail.com>
2025-05-23 13:11:40 +0530

    path issue

--- server/server.js
+++ server/server.js
@@ -52,7 +52,7 @@ const __dirname = path.resolve();
 
 app.use(express.static(path.join(__dirname, "/client/dist")));
 app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
 // Error handler middleware
